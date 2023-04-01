1907ec2d1de15303f9e8423e4c91137ebccd27a5
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-04-01 17:17:43 +1100

    Add sink-gas potential to get total potential with and without softnening potential

--- energies.py
+++ energies.py
@@ -10,8 +10,19 @@ import sarracen as sar
 from rdumpfiles import read_dumpfiles
 from separation import sep_t
 
-path_dumpfiles = '/media/miguelgb/drive2/Dust/Datafiles/A_ccc/'
-dump_list = read_dumpfiles(path=path_dumpfiles)
+
+def p_soft(q):
+    p_soft = 105.*np.power(q,10)/32768. - 11.*np.power(q,9)/256. + 1925.*np.power(q,8)/8192. 
+    p_soft = p_soft - 165.*np.power(q,7)/256. + 825.*np.power(q,6)/1024. - 231.*np.power(q,4)/256. 
+    p_soft = p_soft + 165.*np.power(q,2)/128. - 55./32.
+    return p_soft
+
+def sink_gas_potential(m_sink,h_soft,r):
+    q = r/h_soft
+    p_lowq = p_soft(q[q < 2])*m_sink/h_soft
+    p_highq = -m_sink/r[q > 2]
+    pot = p_lowq.add(p_highq,fill_value=0)
+    return pot
 
 def tot_potential(dumpfile_list):
     time = np.array([])
@@ -30,17 +41,22 @@ def tot_potential(dumpfile_list):
         y_sinks = sdf_sinks['y'][1] - sdf_sinks['y'][0]
         z_sinks = sdf_sinks['z'][1] - sdf_sinks['z'][0]
         r_sinks = np.sqrt(x_sinks**2 + y_sinks**2 + z_sinks**2)
-        r1 = np.sqrt(x1**2 + y1**2 + z1**2)
-        r2 = np.sqrt(x2**2 + y2**2 + z2**2)
+        r1 = np.sqrt(np.square(x1) + np.square(y1) + np.square(z1))
+        r2 = np.sqrt(np.square(x2) + np.square(y2) + np.square(z2))
         phi = - (sdf_sinks['m'][0]/r1).sum() - (sdf_sinks['m'][1]/r2).sum()
+        phi1 = sink_gas_potential(sdf_sinks['m'][0],sdf_sinks['hsoft'][0],r1).sum()
+        phi2 = sink_gas_potential(sdf_sinks['m'][1],sdf_sinks['hsoft'][1],r2).sum()
         pot_bet_sinks = -(sdf_sinks['m'][0]*sdf_sinks['m'][1])/r_sinks
-        #potential = np.append(potential, particlemass*phi + sdf['poten'].sum() + pot_bet_sinks)
-        potential = np.append(potential, pot_bet_sinks)
+        potential = np.append(potential, particlemass*(phi1+phi2) + sdf['poten'].sum() + pot_bet_sinks)
+        #potential = np.append(potential, pot_bet_sinks)
         time = np.append(time,sdf.params['time'])
         print('file: ',file_name)
     return time, potential
 
 if __name__ == "__main__":
+    path_dumpfiles = '/media/miguelgb/drive2/Dust/Datafiles/A_tccclsax_boundmax/'
+    dump_list = read_dumpfiles(path=path_dumpfiles)
     time, sink_potential = tot_potential(dump_list)
     plt.plot(time, sink_potential)
     plt.show()
+
